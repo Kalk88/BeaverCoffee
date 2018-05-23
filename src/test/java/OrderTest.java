92feaae9ef7281977716aeaeae4781182a12f594
marante <bugger@bravis.se>
2018-05-23 11:21:25 +0200

    Fixed update orders, now works perfectly (Simon says so)

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -2,6 +2,7 @@ import app.order.Order;
 import app.order.OrderController;
 import app.order.OrderDao;
 import app.util.Utils;
+import com.google.gson.Gson;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class OrderTest {
     private Morphia morphia;
@@ -154,6 +156,18 @@ public class OrderTest {
         final OrderController controller = new OrderController(dao);
         controller.createOrder(OrderDummy.data);
         assertEquals(5, datastore.getCollection(Order.class).count());
+    }
+
+    @Test
+    public void should_update_order_with_new_values() {
+        final OrderDao dao = new OrderDao(datastore);
+        final OrderController controller = new OrderController(dao);
+        Order orderToBeUpdated = controller.getOrderById("78970117-3715-4f91-8b4f-c4f3342f5a84");
+        Order order = new Gson().fromJson(OrderDummy.updatedData, Order.class);
+        Order updatedOrder = orderToBeUpdated.overwriteOrder(order);
+        assertEquals(updatedOrder.get_id(), orderToBeUpdated.get_id());
+        dao.createOrder(updatedOrder);
+        assertEquals(4, datastore.getCollection(Order.class).count());
     }
 
     // Random tests for the purpose of utility methods and such.
--- src/test/java/test_data/dummy_data/OrderDummy.java
+++ src/test/java/test_data/dummy_data/OrderDummy.java
@@ -11,12 +11,19 @@ public class OrderDummy {
             "status", new String[]{"Finished"}
     );
 
-    public static String data = "{ \"id\": \"78970117-3715-4f91-8b4f\", \"timestamp\": 1526633863, \"discount\": \"%10\", \"customer\":\n" +
+    public static String data = "{ \"id\": \"78970117-3715-4f91-8b4f-c4f3342f5a83\", \"timestamp\": 1526633863, \"discount\": \"%10\", \"customer\":\n" +
             "        { \"id\": \"78970117-3715-4f91-8b4f-c4f3342f5a83\", \"clubmember\":\n" +
             "            {\"id\": \"78970117-3715-4f91-8b4f-c4f3342f5a83\", \"first\": \"Jens\", \"last\": \"Conny\", \"SSN\": \"098745028374\", \"homeAdress\":\n" +
             "                [{\"street\": \"Bogdan Street 10\", \"zip\": \"911 1337\", \"city\": \"Coimbra\", \"country\":\n" +
             "                    {\"code\": \"SE\", \"currency\": \"SEK\", \"language\": \"SV\", \"name\": \"Sverige\"}}], \"occupation\": \"Youtube Influencer\", \"validCountry\":\n" +
             "        {\"code\": \"SE\", \"currency\": \"SEK\", \"language\": \"SV\", \"name\": \"Sverige\"}}}, \"employeeID\": \"12345678-3715-4f91-8b4f-c4f3342f5a83\", \"storeID\": \"69999998-3715-4f91-8b4f-c4f3342f5a83\", \"products\": [ { \"productID\": \"12345678-3715-4f91-8b4f-c4f3342f5a8\", \"quantity\": \"1\"}, { \"productID\": \"12345678-3715-4f91-8b4f-c4f3342f5a83\", \"quantity\": \"1\"} ], \"status\": \"Finished\"}";
+
+    public static String updatedData = "{ \"id\": \"78970117-3715-4f91-8b4f-c4f3342f5a84\", \"timestamp\": 1526633863, \"discount\": \"%10\", \"customer\":\n" +
+            "        { \"id\": \"78970117-3715-4f91-8b4f-c4f3342f5a83\", \"clubmember\":\n" +
+            "            {\"id\": \"78970117-3715-4f91-8b4f-c4f3342f5a83\", \"first\": \"Jens\", \"last\": \"Conny\", \"SSN\": \"098745028374\", \"homeAdress\":\n" +
+            "                [{\"street\": \"Bogdan Street 10\", \"zip\": \"911 1337\", \"city\": \"Coimbra\", \"country\":\n" +
+            "                    {\"code\": \"SE\", \"currency\": \"SEK\", \"language\": \"SV\", \"name\": \"Sverige\"}}], \"occupation\": \"Youtube Influencer\", \"validCountry\":\n" +
+            "        {\"code\": \"SE\", \"currency\": \"SEK\", \"language\": \"SV\", \"name\": \"Sverige\"}}}, \"employeeID\": \"12345678-3715-4f91-8b4f-c4f3342f5a83\", \"storeID\": \"69999998-3715-4f91-8b4f-c4f3342f5a83\", \"products\": [ { \"productID\": \"12345678-3715-4f91-8b4f-c4f3342f5a8\", \"quantity\": \"4\"}, { \"productID\": \"12345678-3715-4f91-8b4f-c4f3342f5a83\", \"quantity\": \"1\"} ], \"status\": \"Finished\"}";
 
     public static Order GetDummyOrder() {
         return new Gson().fromJson(data, Order.class);
