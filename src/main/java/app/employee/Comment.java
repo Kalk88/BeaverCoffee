a9ea754e5d1308def526ef1df2062c014e801d3c
grasshopper <emil.petersson@live.com>
2018-05-23 13:44:17 +0200

    Added functionality for Employee"

---
+++ src/main/java/app/employee/Comment.java
@@ -0,0 +1,23 @@
+package app.employee;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Embedded;
+import org.mongodb.morphia.annotations.Id;
+
+/**
+ * Created by ep on 2018-05-22.
+ */
+@Embedded
+public class Comment {
+    private String employeeID;
+    private int timestamp;
+    private String employerID;
+    private String comment;
+
+    public Comment() {}
+
+    @Override
+    public String toString() {
+        return employeeID  + " " + timestamp  + " " + employerID  + " " + comment.toString();
+    }
+}
--- src/main/java/app/employee/EmployeeDao.java
+++ src/main/java/app/employee/EmployeeDao.java
@@ -1,4 +1,61 @@
 package app.employee;
+import app.order.Order;
+import app.util.Utils;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.query.Query;
+
+import java.util.List;
+import java.util.Map;
 
 public class EmployeeDao {
+    private Datastore datastore;
+
+    public EmployeeDao(Datastore datastore) {
+        this.datastore = datastore;
+    }
+
+    public List<Employee> getAllEmployees() {
+        final Query<Employee> query = datastore.createQuery(Employee.class);
+        final List<Employee> employees = query.asList();
+        return employees;
+    }
+
+    public Employee getEmployeeById(String id) {
+        final Query<Employee> query = datastore.createQuery(Employee.class);
+        final Employee employee = query.field("id").equal(id).get();
+        return employee;
+    }
+
+    public void createEmployee(Employee employee) {
+        datastore.save(employee);
+    }
+
+    public List<Employee> getEmployeesFromQueryParams(Map<String, String[]> params) {
+        final Query<Employee> query = datastore.createQuery(Employee.class);
+
+        params.forEach((k, v) -> {
+            switch(k) {
+                case "from": addStartDateTimestampToQuery(query, v);
+                    break;
+                case "to": addEndDateTimestampToQuery(query, v);
+                    break;
+            }
+        });
+        final List<Employee> employees = query.asList();
+        return employees;
+    }
+
+    private Query<Employee> addStartDateTimestampToQuery(Query<Employee> query, String[] params) {
+        for (String item : params) {
+            query.filter("employee.details.employmentHistory.startDate >=", Utils.getUnixTimestampFromDateString(item));
+        }
+        return query;
+    }
+
+    private Query<Employee> addEndDateTimestampToQuery(Query<Employee> query, String[] params) {
+        for (String item : params) {
+            query.filter("employee.details.employmentHistory.endDate <=", Utils.getUnixTimestampFromDateString(item));
+        }
+        return query;
+    }
 }
